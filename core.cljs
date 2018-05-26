791a335d1c3463a193d57db5dc978d0db8620691
Bobby Towers <porkostomus@mail.com>
2018-05-26 15:52:45 -0700

    Add files via upload

--- core.cljs
+++ core.cljs
@@ -4,17 +4,9 @@
 
 (enable-console-print!)
 
-;; The "database" of your client side UI.
 (def app-state
   (r/atom
-   {:contacts
-    [{:first "Ben" :last "Bitdiddle" :email "benb@mit.edu"}
-     {:first "Alyssa" :middle-initial "P" :last "Hacker" :email "aphacker@mit.edu"}
-     {:first "Eva" :middle "Lu" :last "Ator" :email "eval@mit.edu"}
-     {:first "Louis" :last "Reasoner" :email "prolog@mit.edu"}
-     {:first "Cy" :middle-initial "D" :last "Effect" :email "bugs@mit.edu"}
-     {:first "Lem" :middle-initial "E" :last "Tweakit" :email "morebugs@mit.edu"}]
-    :elements
+   {:elements
 [{:id 1 :name "Hydrogen"}
 {:id 2 :name "Helium"}
 {:id 3 :name "Lithium"}
@@ -125,64 +117,14 @@
 {:id 108 :name "Hassium"}
 {:id 109 :name "Meitnerium"}]}))
 
-(defn update-contacts! [f & args]
-  (apply swap! app-state update-in [:contacts] f args))
-
-(defn add-contact! [c]
-  (update-contacts! conj c))
-
-(defn remove-contact! [c]
-  (update-contacts! (fn [cs]
-                      (vec (remove #(= % c) cs)))
-                    c))
-
-;; The next three fuctions are copy/pasted verbatim from the Om tutorial
-(defn middle-name [{:keys [middle middle-initial]}]
-  (cond
-   middle (str " " middle)
-   middle-initial (str " " middle-initial ".")))
-
-(defn display-name [{:keys [first last] :as contact}]
-  (str last ", " first (middle-name contact)))
-
 (defn display-element [{:keys [id name] :as element}]
   (str id " " name))
 
-(defn parse-contact [contact-str]
-  (let [[first middle last :as parts] (string/split contact-str #"\s+")
-        [first last middle] (if (nil? last) [first middle] [first last middle])
-        middle (when middle (string/replace middle "." ""))
-        c (if middle (count middle) 0)]
-    (when (>= (reduce + (map #(if % 1 0) parts)) 2)
-      (cond-> {:first first :last last}
-        (== c 1) (assoc :middle-initial middle)
-        (>= c 2) (assoc :middle middle)))))
-
-;; UI components
 
 (defn element [c]
   [:li
    [:span (display-element c)]
    ])
-
-(defn contact [c]
-  [:li
-   [:span (display-name c)]
-   [:button {:on-click #(remove-contact! c)} 
-    "Delete"]])
-
-(defn new-contact []
-  (let [val (r/atom "")]
-    (fn []
-      [:div
-       [:input {:type "text"
-                :placeholder "Contact Name"
-                :value @val
-                :on-change #(reset! val (-> % .-target .-value))}]
-       [:button {:on-click #(when-let [c (parse-contact @val)]
-                              (add-contact! c)
-                              (reset! val ""))}
-        "Add"]])))
 
 (defn element-list []
   [:div
@@ -190,14 +132,6 @@
    [:ul
     (for [c (:elements @app-state)]
       [element c])]])
-
-(defn contact-list []
-  [:div
-   [:h1 "Contact list"]
-   [:ul
-    (for [c (:contacts @app-state)]
-      [contact c])]
-   [new-contact]])
 
 ;; Render the root component
 (defn start []
